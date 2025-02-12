5012e63bc83a0700cf2f955510668b445237d8af
Sud0202 <sraghavaraju@adobe.com>
2025-02-12 11:43:03 +0530

    added placeholder

---
+++ blocks/placeholder/placeholder.js
@@ -0,0 +1,10 @@
+import { fetchPlaceholders } from '../../scripts/aem.js';
+
+export default async function decorate(block) {
+
+    const placeholders = await fetchPlaceholders('');
+
+    console.log('Placeholders JSON:', placeholders);
+
+ 
+}
